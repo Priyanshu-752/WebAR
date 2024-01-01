e66c60ece898985248d81495d17cbcf2e757dbc6
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2024-01-01 11:38:46 +0530

    Updates

--- src/components/xr-gallery/Druid.jsx
+++ src/components/xr-gallery/Druid.jsx
@@ -37,18 +37,3 @@ export default function Druid(props) {
 }
 
 useGLTF.preload("/models/druid.gltf");
-
-// import { useLoader } from "@react-three/fiber";
-// import { Suspense } from "react";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-// const Model = ({ position }) => {
-//   const gltf = useLoader(GLTFLoader, "/models/druid.gltf");
-//   return (
-//     <Suspense fallback={null}>
-//       <primitive position={position} object={gltf.scene} />
-//     </Suspense>
-//   );
-// };
-
-// export default Model;
